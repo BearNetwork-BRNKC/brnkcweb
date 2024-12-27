6ceeb5eab6e6d83fd86c0a34fe784074a2db1069
BearNetwork <105615886+BearNetwork-BRNKC@users.noreply.github.com>
2024-12-27 23:13:00 +0800

    Add files via upload

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,8 +10,8 @@ const Billing = () => (
       className="w-[90%] h-[80%] relative z-[5]"
     />
       {/* gradient start */}
-      <div className="absolute z-[3] -left-1/2 top-0 w-[70%] h-[40%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[70%] h-[40%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      <div className="absolute z-[3] -left-1/2 top-0 w-[80%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[80%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
 
