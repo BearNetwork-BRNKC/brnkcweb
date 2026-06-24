2a5df284724a776f8e2ca3a849eb227ecba26a10
BearNetwork-BRNKC <bnkt@bearnetwork.net>
2026-06-24 14:40:07 +0800

    更新

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,8 +10,8 @@ const Billing = () => (
       className="w-[90%] h-[80%] relative z-[5]"
     />
       {/* gradient start */}
-      <div className="absolute z-[3] -left-1/2 top-0 w-[80%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[80%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      <div className="absolute z-[3] -left-1/2 top-0 w-[80%] h-[65%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[80%] h-[65%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
 
